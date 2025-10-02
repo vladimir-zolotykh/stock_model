cf3036adb7a69c49fcb294b5529d9a470790e81f
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-10-02 10:14:59 +0300

    Add DirectorySize, Directory
    A descriptor's warmup: Directory is the descriptor, Directory - class to
    use it.

--- run_stock.py
+++ run_stock.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import os
+
+
+class DirectorySize:
+    def __get__(self, instance, owner=None):
+        return len(os.listdir(instance._dirname))
+
+
+class Directory:
+    size = DirectorySize()
+
+    def __init__(self, dirname):
+        self._dirname = dirname
+
+    @property
+    def dirname(self):
+        return self._dirname
 
 
 if __name__ == "__main__":
-    pass
+    d1 = Directory("/etc/")
+    d2 = Directory(os.path.expanduser("~/Documents/projects/python/"))
+    print(d1.dirname, d1.size)
+    print(d2.dirname, d2.size)
