2e093b84c318ab283c344ec72839a37b969124f0
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-10-06 19:37:50 +0300

    Initial import
    Descriptor classes (Descriptor, Typed, Sized), specific descriptor-types
    (String, SizedString, UnsignedIteger, etc. )

---
+++ stock_acme.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+import unittest
+
+
+class Descriptor:
+    def __set_name(self, owner, name):
+        self.name = "_nad_" + name  # not a descriptor
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        setattr(instance, self.name, value)
+
+
+class Sized(Descriptor):
+    def __init__(self, minsize=0, maxsize=None):
+        super().__init__()
+        self.minsize = minsize
+        self.maxsize = maxsize
+
+    def __set__(self, instance, value):
+        if isinstance(self.minsize, (int, float)) and len(value) < self.minsize:
+            raise ValueError(f"{value!r} must not be shorter than {self.minsize}")
+        if isinstance(self.maxsize, (int, float)) and len(value) > self.maxsize:
+            raise ValueError(f"{value!r} must not be longer than {self.maxsize}")
+        super().__set__(instance, value)
+
+
+class Typed(Descriptor):
+    expected_type = type(None)
+
+    def __set__(self, instance, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError(f"{value!r} must be of type {self.expected_type}")
+        super().__set__(instance, value)
+
+
+class String(Typed):
+    expected_type = str
+
+
+class Integer(Typed):
+    expected_type = int
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class SizedString(String, Sized):
+    pass
+
+
+class UnsignedInteger(Integer, Sized):
+    pass
+
+
+class UnsignedFloat(Float, Sized):
+    pass
+
+
+class Stock:
+    name = SizedString(maxsize=8)
+    shares = UnsignedInteger()
+    price = UnsignedFloat()
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.share = shares
+        self.price = price
+
+
+class TestMaxsize(unittest.TestCase):
+    def test_stock(self):
+        Stock("ACME", 50, 91.1)
+
+
+if __name__ == "__main__":
+    unittest.main()
